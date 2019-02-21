e2c705c8a0ef5114ce7bd8fb8311f341f68d7e4a
Xuri Li <dlshle@hotmail.com>
2019-02-21 00:00:10 -0600

    figuring out the new GA solver

--- nqueens.js
+++ nqueens.js
@@ -97,3 +97,100 @@ function validate(board, p){
 
 	return true;
 }
+
+/*
+ * Genetic algorithm solver of the n queen problem
+ * USE insertSample only!
+ * sample = {sample:sample, fitness:fitness}
+ */
+function gaSolver(boardSize){
+	//original population
+	let population = generateSamples(10, boardSize);
+
+	//TODO:choose best n samples and cross-over
+
+	//TODO:randomly choose n of the cross-overed sample and mutate them randomly
+	
+}
+
+function generateSamples(n, size){
+	let population = [];
+	while(set.size<n){
+		let sample = generateRandList(size);
+		let fitness = getFitness(sample);
+		insertSample({sample:sample, fitness:fitness});
+	}
+	return population;
+}
+
+function insertSample(sample, list){
+	list.push(sample);
+	let i = list.length-1;
+	while(i>0){
+		if(list[i].fitness>list[i-1].fitness){
+			let temp = list[i];
+			list[i] = list[i-1];
+			list[i-1] = temp;
+		}
+		i--;
+	}
+}
+
+function generateRandNumber(low, high){
+	return Math.floor(Math.random() *high)+low;
+}
+
+function generateRandList(len){
+	let list = [];
+	for(let i=0;i<len;i++)
+		list.push(generateRandNumber(0, len-1);
+	return list;
+}
+
+/*
+ * getFitness calculates number of conflictions
+ * fitness = board_size*board_size - #conflictions
+ */
+function getFitness(sample){
+	return getNumConflictionsOnBoard(sample)/(sample.length*sample.length);
+}
+
+function getNumConflictionsOnBoard(board){
+	let result = 0;
+	for(let i=0;i<borad.length;i++){
+		result+=getNumberConflictions(board, i);
+	}
+	return result;
+}
+
+function getNumConflictions(board, p){
+	let result = 0;
+	//up-down
+	for(let i=0;i<board.length;i++){
+		if(i==p)
+			continue;
+		if(board[i]==board[p])
+			result++;
+	}
+
+	//diagnoal
+	for(let i=0;i<p;i++){
+		if(board[i]==(board[p]+(p-i))||board[i]==(board[p]-(p-i)))
+			result++;
+	}
+	for(let i=p+1;i<board.length;i++){
+		if(board[i]==(board[p]+(i-p))||board[i]==(board[p]-(i-p)))
+			result++;
+	}
+
+	return result;
+}
+
+function crossOver(sample0, sample1){
+	//TODO:randomly choose a position and intersect two samples to generate one new sample
+	//123|456789 & 987|654321 => 123654321
+}
+
+function getMutatedSample(sample){
+	//TODO:randomly choose one position and assign a random number to that position
+}
